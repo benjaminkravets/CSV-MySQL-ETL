32b24e83889b75b7dd439784887b3b4d04e6580d
benjaminkravets <70412290+benjaminkravets@users.noreply.github.com>
2022-10-10 10:07:30 -0400

    Add files via upload

---
+++ db.py
@@ -0,0 +1,35 @@
+
+# importing module
+
+import cx_Oracle
+ 
+# Create a table in Oracle database
+try:
+ 
+    con = cx_Oracle.connect('SYSTEM/1@localhost:1521/xe')
+    print(con.version)
+ 
+    # Now execute the sqlquery
+    cursor = con.cursor()
+ 
+    # Creating a table 
+    
+    #cursor.execute(
+    #    "create table employee(empid integer primary key, name varchar2(30), salary number(10, 2))")
+    cursor.execute("select * from employee")
+
+    row = cursor.fetchone()
+    print(row)
+
+    print("Table Created successfully")
+ 
+except cx_Oracle.DatabaseError as e:
+    print("There is a problem with Oracle", e)
+ 
+# by writing finally if any error occurs
+# then also we can close the all database operation
+finally:
+    if cursor:
+        cursor.close()
+    if con:
+        con.close()
