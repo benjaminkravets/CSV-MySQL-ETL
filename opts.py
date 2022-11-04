858dad8f98f1bba8e1d6a2c4fbd80f3627a716f7
benjaminkravets <bakravets@gmail.com>
2022-11-04 15:56:53 -0400

    cleanup

--- opts.py
+++ opts.py
@@ -21,15 +21,31 @@ for opt, arg in options:
     elif opt in ('-i', '--input'):
         INPUT_FILE = arg
    
-   
+
 # connecting to the database
 dataBase = mysql.connector.connect(
                      host = "127.0.0.1",
                      user = "root",
                      password = "fonz",
                      database = "world" )
-                     
+   
+# preparing a cursor object
 cursorObject = dataBase.cursor()
+   
+# selecting query
+query = "SELECT ID FROM CITY"
+query += " LIMIT 10"
+print(query)
+
+cursorObject.execute(query)
+ 
+myresult = cursorObject.fetchall()
+ 
+for x in myresult:
+    print(x)
+ 
+# disconnecting from server
+dataBase.close()
 
 
 
