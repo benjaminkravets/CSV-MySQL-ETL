5cce5733c54794c0037cc913ed7be17211616d41
benjaminkravets <bakravets@gmail.com>
2022-12-08 12:29:22 -0500

    pylint

--- CSV_ETL.py
+++ CSV_ETL.py
@@ -1,11 +1,7 @@
-from email import header
 import getopt
 import sys
 import csv
 import mysql.connector
-from os import listdir
-
-
 
 VERBOSE = False
 INPUT_FILE = ''
@@ -21,54 +17,34 @@ for opt, arg in options:
         VERBOSE = True
     elif opt in ('-i', '--input'):
         INPUT_FILE = arg
-   
-
 # connecting to the database
 dataBase = mysql.connector.connect(
                      host = "127.0.0.1",
                      user = "root",
                      password = "fonz",
                      database = "world" )
-   
 # preparing a cursor object
 cursorObject = dataBase.cursor()
-   
 # selecting query
 query = "SELECT ID FROM CITY"
 query += " LIMIT 10"
 print(query)
-
-#cursorObject.execute(query)
- 
-#myresult = cursorObject.fetchall()
- 
-#for x in myresult:
-#    print(x)
- 
+cursorObject.execute(query)
+myresult = cursorObject.fetchall()
+for x in myresult:
+    print(x)
 # disconnecting from server
 dataBase.close()
-
-
-
-
 with open(INPUT_FILE) as CSV_FILE:
     CSV_READER = csv.reader( x.replace('\0','') for x in CSV_FILE)
-    
     headers = [x.strip() for x in next(CSV_READER)]
     table = input("Which table should the data be added to?: ")
-
-
     for row in CSV_READER:
         if row:
             d = dict(zip(headers, map(str.strip, row)))
             insert_query = "INSERT INTO " + table + " VALUES "
-            
             for i in range(len(row)-1):
                 insert_query += ("'" + row[i] + "',")
             insert_query +=("'" + row[len(row)-1] + "'")
-                
             print(insert_query)
-
-
-
-
+            
