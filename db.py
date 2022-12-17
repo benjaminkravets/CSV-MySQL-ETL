7dcd7b81347d73cc1c62a279662c981866d47981
benjaminkravets <bakravets@gmail.com>
2022-12-16 21:04:11 -0500

    docstring

--- db.py
+++ db.py
@@ -1,6 +1,6 @@
 # import OS module
+"""List directory items"""
 import os
-
 # Get the list of all files and directories
 path = "C://Users//bakra//Desktop"
 dir_list = os.listdir(path)
@@ -8,4 +8,8 @@ dir_list = os.listdir(path)
 print("Files and directories in '", path, "' :")
 
 # prints all files
-print(dir_list)
+#print(dir_list)
+
+#print(type(dir_list))
+for x in dir_list:
+    print(x)
